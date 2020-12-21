26f9498a9a9a15d2c17c4f9dfc56139c07a6dd68
NilE <nilenon@gmail.com>
2020-12-21 22:08:24 +0700

    [Add] Cursur

--- client/src/components/SlipPayment.jsx
+++ client/src/components/SlipPayment.jsx
@@ -2,7 +2,7 @@ import CloseRoundedIcon from '@material-ui/icons/CloseRounded'; import { color }
 const SlipPayment = ({ image, store, onDeletedImage }) => {
   return (
     <div style={{ paddingTop: 10, position: 'relative' }}>
-      <CloseRoundedIcon style={{ position: 'absolute', left: 45, top: 1, color: color[store] }} size="small" onClick={onDeletedImage} />
+      <CloseRoundedIcon style={{ position: 'absolute', left: 45, top: 1, color: color[store], cursor: 'pointer' }} size="small" onClick={onDeletedImage} />
       <img src={image} alt="slip" width={60} height={100} style={{ border: '3px solid white' }} />
     </div>
   )
